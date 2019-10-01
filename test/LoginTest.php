78ff93d8f741b2eb40c0dad38dfb83a53f13f542
Jerome <jerome@macappstudio.com>
2019-10-01 21:59:41 +0530

    New Login php file updated

--- test/LoginTest.php
+++ test/LoginTest.php
@@ -3,21 +3,20 @@ use PHPUnit\Framework\TestCase;
 
 class LoginTest{
 
-  public function testsignin(){
-    $email ='Jerome';
-    $password  ='12345';
-    if($email != '' && $password !='')
+
+    public function testEmpty()
     {
+        $email ='Jerome';
+        $password  ='12345';
+        $this->assertEmpty($email);
+        $this->assertEmpty($password);
+        return $value;
+    }
+
+    public function testsignin(){
     $this->assertEquals($email , 'Jerome');
     $this->assertEquals($password , '12345');
     }
-    else
-    {
-     $this->assertEquals($email, '');   
-     $this->assertEquals($password, ''); 
-    }
-
-  }
   
 }
 ?>
