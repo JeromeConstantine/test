bbbee42c4a3e9b9d6e0b757276779770fb56d3b4
Jerome <jerome@macappstudio.com>
2019-10-08 15:21:16 +0530

    most files are deleted and updated

--- test/MyGuestbookTest.php
+++ test/MyGuestbookTest.php
@@ -51,28 +51,4 @@ class MyGuestbookTest extends TestCase
   }
  
 }
-// use PHPUnit\Framework\TestCase;
-// use PHPUnit\DbUnit\TestCaseTrait;
-
-// class MyGuestbookTest extends TestCase
-// {
-//     use TestCaseTrait;
-
-//     /**
-//      * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
-//      */
-//     public function getConnection()
-//     {
-//         $pdo = new PDO('sqlite::memory:');
-//         return $this->createDefaultDBConnection($pdo, ':memory:');
-//     }
-
-//     /**
-//      * @return PHPUnit_Extensions_Database_DataSet_IDataSet
-//      */
-//     public function getDataSet()
-//     {
-//         return $this->createFlatXMLDataSet(dirname(__FILE__).'/guestbook-seed.xml');
-//     }
-// }
 ?>
